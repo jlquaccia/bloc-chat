c4c00768d984e01f022eac05b6bf5be83c0dac86
Jason Quaccia <jlquaccia@gmail.com>
2016-06-01 15:34:34 -0700

    built basic functionality for modal to add a new chat room.  integrating it with firebase in progress

--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -1,6 +1,25 @@
 (function() {
     function HomeCtrl($scope, Room) {
         $scope.allRooms = Room.all;
+        
+//        $scope.newRoom = {
+//            name: ''
+//        };
+//        
+//        $scope.addRoom = function() {
+//            $scope.allRooms.$add($scope.roomName).then(function(rooms) {
+//                var id = rooms.key();
+//                
+//                console.log("added record with id " + id);
+//                $scope.allRooms.$indexFor(id);
+//            });
+//        };
+        
+        // Bootstrap Modal
+        $scope.showModal = false;
+        $scope.toggleModal = function() {
+            $scope.showModal = !$scope.showModal;
+        };
     }
     
     angular
