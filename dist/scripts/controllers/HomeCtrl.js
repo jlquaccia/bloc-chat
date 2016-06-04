bc03faeb152cc5c272c35cbc9c4222679ee4a493
Jason Quaccia <jlquaccia@gmail.com>
2016-06-03 21:01:09 -0700

    checkpoint 3 completed

--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -1,21 +1,20 @@
 (function() {
     function HomeCtrl($scope, Room) {
         $scope.allRooms = Room.all;
-//        
-//        $scope.addRoom = function() {
-//            // Creating a unique id
-//            var timestamp = new Date().valueOf();
-//
-//            $scope.allRooms.$add({
-//                id: timestamp,
-//                name: $scope.roomName
-//            });
-//
-//            $scope.roomName = '';
-//        };
+        
+        $scope.addRoom = function() {
+            // Creating a unique id
+            var timestamp = new Date().valueOf();
+
+            $scope.allRooms.$add({
+                id: timestamp,
+                name: $scope.roomName
+            });
+
+            $scope.roomName = '';
+        };
         
         // Overlay
-        
         $scope.toggleOverlay = function() {
             if (!$('.overlay-hugeinc').hasClass('open')) {
                 $('.overlay-hugeinc').addClass('open');
@@ -23,6 +22,13 @@
                 $('.overlay-hugeinc').removeClass('open');
             }
         }
+        
+        // Autofocus for new room form
+        $scope.autoFocus = function() {
+            if ($('.overlay').hasClass('open')) {
+                $('input.overlay_input').focus();
+            }
+        };
     }
     
     angular
