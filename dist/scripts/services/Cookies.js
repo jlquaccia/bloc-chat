83746e841589e273da75806dafab97cde844511b
Jason Quaccia <jlquaccia@gmail.com>
2016-06-09 19:06:29 -0700

    enabled modal to fade in on initial app load if a user has not set a username yet. prompts the user to set their username

--- dist/scripts/services/Cookies.js
+++ dist/scripts/services/Cookies.js
@@ -2,13 +2,17 @@
     function Cookies($cookies) {
         var currentUser = $cookies.get('blocChatCurrentUser');
         
+        // Will prompt a user on app load to set a username if they have not already
         if (!currentUser || currentUser === '') {
-            // Modal test
-            $('#modal-1').addClass('md-show');
+            console.log('there is no current user set');
+            
+            setTimeout(function() {
+                $('#modal-1').addClass('md-show');
 
-            $('.md-close').click(function() {
-              $('#modal-1').removeClass('md-show');
-            });
+                $('.md-close').click(function() {
+                  $('#modal-1').removeClass('md-show');
+                });
+            }, 500);
         }
     }
     
